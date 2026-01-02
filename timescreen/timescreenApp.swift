fea15ff6c033a932b317c8b57c0a4b5175f5d2e0
ShukriPro <justshukri078@gmail.com>
2026-01-02 21:14:05 +1300

    Initial Commit

---
+++ timescreen/timescreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  timescreenApp.swift
+//  timescreen
+//
+//  Created by Shukri Ali on 02/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct timescreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
